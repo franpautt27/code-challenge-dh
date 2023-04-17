42977b41867d050361e92b49da2357667ec8ab91
Francisco Pautt <franpautt@gmail.com>
2023-04-17 14:57:16 -0500

    API call link corrected

--- services/products.js
+++ services/products.js
@@ -2,7 +2,7 @@ import api from './api';
 
 export async function getProducts(){
     try {
-        const products = await api.get("/product")
+        const products = await api.get("/products")
         return products.data
     } catch (err) {
         throw new Error(err.message)
